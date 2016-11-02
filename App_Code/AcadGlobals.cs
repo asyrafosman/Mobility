20d13cb171db719c4ff020b9c9766fb1468d6bb6
Mohamad Asyraf Osman <mesaasyraf@yahoo.com>
2016-11-02 11:26:46 +0800

    include all changes

---
+++ App_Code/AcadGlobals.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Global
+/// </summary>
+public class AcadGlobals
+{
+    public const string SCHEME_AIMS = "AIMS";
+    public const string SCHEME_SPSWEB = "SPSWEB";
+    public const string URL_ACAD = "http://academic2.utm.my/";
+    public const string URL_NFS = "ftp://datastore.utm.my";
+    public const string NFS_UID = "smb-user-academic";
+    public const string NFS_PID = "smb-u53r-academic";
+    public const string NFS_PATH = "/academic-attachment/acadnfs/";
+    
+    //ToolTip - added by Wan Mat on 29 May 2016
+    public const string TOOLTIP_ADD_NEW_RECORD = "Click here to add new record.";
+    public const string TOOLTIP_INSERT_RECORD = "Click here to save record.";
+    public const string TOOLTIP_UPDATE_RECORD = "Click here to update record.";
+    public const string TOOLTIP_DELETE_RECORD = "Click here to delete record.";
+    public const string TOOLTIP_CANCEL = "Click here to cancel this operation";
+    public const string TOOLTIP_FIND_RECORD = "Click here to start searching.";
+    
+    //Icon - added by Wan Mat on 29 May 2016
+    public const string ICON_FIND_RECORD = "~/Styles/images/find_icon_red.png";
+    public const string ICON_GRIDVIEW_LIST_RECORD = "~/Styles/images/rec_list.png";
+    public const string ICON_GRIDVIEW_VIEW_RECORD = "~/Styles/images/rec_edit.png";
+    public const string ICON_GRIDVIEW_DELETE_RECORD = "~/Styles/images/rec_delete.png";
+    public const string ICON_RECORD_ADD_NEW = "~/Styles/images/rec_add_button.png"; //"<i class=\"fa fa-plus-circle\" style=\"color: green; font-size: 14pt;\"></i>";
+    public const string ICON_RECORD_SAVE = "~/Styles/images/rec_save_button.png"; //"<i class=\"fa fa-plus-circle\" style=\"color: green;\"></i>";
+    public const string ICON_RECORD_UPDATE = "~/Styles/images/rec_update_button.png"; //"<i class=\"fa fa-plus-circle\" style=\"color: green;\"></i>";
+    public const string ICON_RECORD_DELETE = "~/Styles/images/rec_delete_button.png"; //"<i class=\"fa fa-plus-circle\" style=\"color: green;\"></i>";
+    public const string ICON_CANCEL = "~/Styles/images/rec_cancel_button.png"; //"<i class=\"fa fa-plus-circle\" style=\"color: green;\"></i>";
+    
+    //CRUD operation alert message - added by Wan Mat on 29 May 2016
+    public const string RECORD_INSERT_SUCCESS = "alert('Record saved successfully.');";
+    public const string RECORD_INSERT_FAILED = "alert('Save record failed.');";
+    public const string RECORD_UPDATE_SUCCESS = "alert('Record updated successfully.');";
+    public const string RECORD_UPDATE_FAILED = "alert('Update record failed.');";
+    public const string RECORD_DELETE_SUCCESS = "alert('Record deleted successfully.');";
+    public const string RECORD_DELETE_FAILED = "alert('Delete record failed.');";
+	
+	//Account Status
+    public const string STUDENT_HAVE_DEBT = "alert('You have debt. \\nPlease refer to Bursar Office.');";
+    public const string STUDENT_HAS_DEBT = "alert('You has debt. \\nPlease refer to Bursar Office.');";
+}
---
+++ UGStudent/UGMasterPage.master.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OracleClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+public partial class Student_UGMasterPage : System.Web.UI.MasterPage
+{
+    OracleConnection oraCon = new OracleConnection(System.Configuration.ConfigurationManager.ConnectionStrings["SMUTM"].ConnectionString);
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Session["acadUserSs"] = "201620171";
+        Session["acadUserNm"] = "AIR KELAPA BIN KELABU ASAP";
+        Session["acadUserPr"] = "Bachelor Of Computer Science (Database Systems)";
+        Session["acadUserFn"] = "Computing";
+        Session["acadUserMt"] = "A14CS0999";
+        Session["acadUserBs"] = "6";
+        Session["acadUserNs"] = "8";
+        Session["acadUserSv"] = "Dr. Gigi Sweetiee";
+        Session["acadUserTs"] = "Taught Course";
+        Session["acadUserEm"] = "kaakasim@live.utm.my";
+        Session["acadUserTl"] = "017-754943283";
+
+
+
+		if (!IsPostBack)
+		{
+			string sesisem = Session["acadUserSs"].ToString();
+			showProfile();
+            checkDebt("");
+            showAchievement("", "");
+            showActivities("");
+			lblLoggedInUser.Text = Session["acadUserNm"].ToString();
+		}
+    }
+    protected void AC_Click(object sender, ImageClickEventArgs e)
+    {
+        Server.Transfer("frmAcadCalendar.aspx", false);
+    }
+
+    protected void EA_Click(object sender, ImageClickEventArgs e)
+    {
+        Server.Transfer("frmListHEMA.aspx", false);
+    }
+
+    protected void SD_Click(object sender, ImageClickEventArgs e)
+    {
+        Server.Transfer("searchStaff.aspx", false);
+    }
+
+    protected void EM_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("https://login.microsoftonline.com/login.srf?wa=wsignin1.0&rpsnv=4&ct=1458028969&rver=6.6.6556.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.office365.com%2fowa%2fmain.php%3frealm%3dlive.utm.my%26vd%3dmail&id=260563&whr=live.utm.my&CBCXT=out&msafed=0");
+    }
+
+    protected void UL_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://library.utm.my/");
+    }
+
+    protected void AU_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("frmAskUs.aspx", true);
+    }
+
+    protected void HO_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://hostel.utm.my", true);
+    }
+
+    protected void BU_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://ebendahari.utm.my/bendahari/", true);
+    }
+
+    protected void FA_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect(AcadGlobals.URL_ACAD + "UGStudent/AccountStatus.aspx", false);
+    }
+
+    protected void AW_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://pgs.utm.my/awd56v2/", true);
+    }
+
+    protected void RE_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect(AcadGlobals.URL_ACAD + "PGStudent/CV.aspx", true);
+    }
+
+    protected void CQ_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://aimsweb.utm.my/kokurikulum", true);
+    }
+
+    protected void EX_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("https://pure.utm.my/", true);
+    }
+
+    protected void ADU_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://aduan.utm.my/", true);
+    }
+
+    protected void ITS_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://its.utm.my", true);
+    }
+
+    protected void EP_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://aimsweb.utm.my/eppp/index.asp");
+    }
+
+    protected void EPO_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://eportfolio.utm.my");
+    }
+
+    protected void EL_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("http://elearning.utm.my");
+    }
+
+    /**
+      * Function  : showProfile() 
+      * Remarks   : To Show Student's Profile
+      * Date      : 31/10/2016
+      * Developer : Mohd Azman
+      **/
+    protected void showProfile()
+    {
+        imgPhoto.InnerHtml = "<img src=\"../Styles/images/nophoto.png\" class=\"img-profile\" width=\"100\" alt=\"profileimage\" />";
+        lblName.Text = Session["acadUserNm"].ToString();
+        lblProgramme.Text = Session["acadUserPr"].ToString();
+        lblFaculty.Text = Session["acadUserFn"].ToString();
+        lblMatric.Text = Session["acadUserMt"].ToString();
+        lblBilSemester.Text = Session["acadUserBs"].ToString() + " / " + Session["acadUserNs"].ToString();
+        lblAA.Text = Session["acadUserSv"].ToString();
+        lblToS.Text = Session["acadUserTs"].ToString() + " (Full Time)";
+        lblEmail.Text = Session["acadUserEm"].ToString();
+        lblPhone.Text = Session["acadUserTl"].ToString();
+    }
+
+    /**
+       * Function  : checkDebt() 
+       * Remarks   : To Show Student's Debt
+       * Date      : 31/10/2016
+       * Developer : Mohd Azman
+       **/
+    protected void checkDebt(string nokp)
+    {
+        pnlDebt.Visible = true;
+        lblDebt.Text = "RM 200.50";
+    }
+
+    /**
+       * Function  : showAchievement() 
+       * Remarks   : To Show Student's Num of Achievement
+       * Date      : 31/10/2016
+       * Developer : Mohd Azman
+       **/
+    protected void showAchievement(string sesisem, string nokp)
+    {
+	    lblResult.Text = "3.80";
+    }
+
+    /**
+       * Function  : showActivities() 
+       * Remarks   : To Show Student's Num of Activities
+       * Date      : 31/10/2016
+       * Developer : Mohd Azman
+       **/
+    protected void showActivities(string matrik)
+    {
+        lblActivity.Text = "20";
+    }
+
+
+ 
+
+}
